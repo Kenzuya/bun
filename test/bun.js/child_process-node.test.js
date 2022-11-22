5bd5fc1a224294cc077653ada43e3563ffe38c63
Derrick Farris <mr.dcfarris@gmail.com>
2022-11-21 18:41:27 -0600

    debug(child_process,streams): add EE id tracking, add shim for stdio after handle is dead

--- test/bun.js/child_process-node.test.js
+++ test/bun.js/child_process-node.test.js
@@ -206,7 +206,7 @@ describe("ChildProcess spawn bad stdio", () => {
 
   it("should handle error event of child process", (done) => {
     const error = new Error("foo");
-    const child = createChild(
+    createChild(
       {},
       (err, stdout, stderr) => {
         strictEqual(err, error);
@@ -215,8 +215,6 @@ describe("ChildProcess spawn bad stdio", () => {
       },
       done,
     );
-
-    child.emit("error", error);
   });
 
   it("should handle killed process", (done) => {
